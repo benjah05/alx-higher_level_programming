8df217a4169c367b192ec412acf4d71847371371
benjah05 <senzelebenjamine@gmail.com>
2024-09-16 03:58:55 -0700

    Handle separators

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix or not matrix[0]:
         print()
     else:
-        size = len(matrix)
         for row in matrix:
             for j in range(len(row)):
-                print("{:d}".format(row[j]), end=" " if j < size - 1 else "\n")
+                print("{:d}".format(row[j]), end=" " if j < len(row) - 1 else "\n")

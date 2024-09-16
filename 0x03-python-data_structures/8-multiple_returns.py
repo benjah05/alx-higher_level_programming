0934b499e7d07b90ebd29f7480a66d7d7fde08e3
benjah05 <senzelebenjamine@gmail.com>
2024-09-16 03:22:14 -0700

    Handle empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        sentence[0] = None
-    tuple_sentence = (len(sentence), sentence[0])
+        first_char = None
+    else:
+        first_char = sentence[0]
+    tuple_sentence = (len(sentence), first_char)
     return (tuple_sentence)

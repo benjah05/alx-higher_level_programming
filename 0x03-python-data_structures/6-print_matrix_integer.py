eb4f7c3594de7d09db7a8cc0a9cdc7318526c02a
benjah05 <senzelebenjamine@gmail.com>
2024-09-16 02:37:17 -0700

    Handle non-square matrices

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     size = len(matrix)
     try:
-        for i in range(size):
-            for j in range(size):
+        for i in matrix:
+            for j in range(len(i)):
                 print("{:d}".format(matrix[i][j]), end=" " if j < size - 1 else "\n")
     except:
         print()

40b65af69e0dc2acd97f4af57b51265e20480acd
benjah05 <senzelebenjamine@gmail.com>
2024-09-16 03:15:07 -0700

    Handle empty list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 def divisible_by_2(my_list=[]):
     if not my_list:
         return (my_list)
-    iseven = my_list[:]
-    for i in iseven:
+    iseven = []
+    for i in my_list:
         if i % 2 == 0:
             iseven[i] = True
         else:

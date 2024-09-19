53e6037fe588ce1995cbf2eee9542737e28f3536
benjah05 <senzelebenjamine@gmail.com>
2024-09-18 17:50:47 -0700

    Roman Numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,12 +5,8 @@ def roman_to_int(roman_string):
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
             'D': 500, 'M': 1000}
     integer = 0
-    prev = 0
     for r in roman_string:
         for k, v in roman_numerals.items():
-            if r > prev
-                integer += r - 2 * prev
-            else:
-                integer += k
-            prev = r
+            if r == k:
+                integer += v
     return (integer)

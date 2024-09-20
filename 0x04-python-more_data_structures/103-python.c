25c6c74fe0d6fb4dbd48b6e7f285982c38303bb1
benjah05 <senzelebenjamine@gmail.com>
2024-09-20 16:31:08 -0700

    Palindromes

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,11 @@
+#include "Python.h"
+#include <stdio.h>
+/**
+ * print_python_bytes - print basic info about Python lists
+ * and Python bytes objects
+ * @p: bytes object
+ * Return: void
+ */
+void print_python_bytes(PyObject *p)
+{
+}

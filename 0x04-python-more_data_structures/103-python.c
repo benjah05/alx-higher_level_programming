801ce7cf8ce7bddd1a4de1327ffddaffcae47612
benjah05 <senzelebenjamine@gmail.com>
2024-09-22 11:53:44 -0700

    CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,6 @@
 #include "Python.h"
-#include <stdio.h>
+#include <object.h>
+#include <listobject.h>
 /**
  * print_python_bytes - print basic info about Python lists
  * and Python bytes objects
@@ -8,4 +9,16 @@
  */
 void print_python_bytes(PyObject *p)
 {
+	Py_ssize_t size, allocated, i;
+	PyObject *item;
+
+	size = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", allocated);
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element: %zd: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }

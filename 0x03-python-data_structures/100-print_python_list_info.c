df18f97ff80fc987aefc2435eaea3df3eceec2ff
benjah05 <senzelebenjamine@gmail.com>
2024-09-22 12:07:02 -0700

    CPython #1: PyBytesObject

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -19,6 +19,6 @@ void print_python_list_info(PyObject *p)
 	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
-		printf("Element: %zd: %s\n", i, Py_TYPE(item)->tp_name);
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }

9eb2a7b91e9af49d22952cfc5a02e19781586d40
benjah05 <senzelebenjamine@gmail.com>
2024-09-30 13:05:25 -0700

    Avoid Bare except

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         c = a / b
-    except TypeError:
+    except ValueError:
         c = "None"
     finally:
         print("Inside result: {}".format(c))

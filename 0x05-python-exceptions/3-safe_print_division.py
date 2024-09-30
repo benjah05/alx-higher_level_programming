8b3d7ba6d11a68f40c40a8425118334b1a7eb506
benjah05 <senzelebenjamine@gmail.com>
2024-09-30 13:09:47 -0700

    Handle apt errors

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         c = a / b
-    except ValueError:
+    except (TypeError, ZeroDivisionError):
         c = "None"
     finally:
         print("Inside result: {}".format(c))

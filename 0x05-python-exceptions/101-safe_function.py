24eeaefce4627e609ad3be2462d315369e37325f
benjah05 <senzelebenjamine@gmail.com>
2024-09-30 15:53:56 -0700

    Safe integer print with error message, Safe function & ByteCode -> Python #4

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except (RuntimeError, IndexError, ZeroDivisionError) as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        result = None
+    return (result)

16ebece1b5849cb5fed9845c03106e0e4d3203e1
benjah05 <senzelebenjamine@gmail.com>
2024-10-01 12:03:27 -0700

    Python documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,8 +17,8 @@ class Square:
             TypeError: size not an integer
             ValueError: size is less than 0
         """
-        if (isinstance(size, int) == False):
+        self.__size = size
+        if (isinstance(self.__size, int) == False):
             raise TypeError("size must be an integer")
-        if (size < 0):
+        if (self.__size < 0):
             raise ValueError("size must be >= 0")
-        self.__size == size

a52c98b3e3cd721594b62e15a646900f9157a900
benjah05 <senzelebenjamine@gmail.com>
2024-10-01 11:56:58 -0700

    Python documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,15 +8,13 @@ class Square:
         __size (int): The size of the square's side
     """
     def __init__(self, size=0):
-        """Initialize a new square
+        """Initialize a new squarei
 
         Args:
             size (int): The square's side
-
         Raises:
             TypeError: size not an integer
             ValueError: size is less than 0
-
         """
         self.__size = size
         if (isinstance(self.__size, int) == False):

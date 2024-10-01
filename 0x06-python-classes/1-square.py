8c7b8a78b547cf60a1f5a4aac054b60c8effce98
benjah05 <senzelebenjamine@gmail.com>
2024-10-01 11:17:35 -0700

    Python documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 """Class square intializes private instance 'size'"""
+
+
 class Square:
+    """Define a square"""
+
+
     def __init__(self, size):
+        """Initialize a new square
+        Args:
+            size (int): The size of the new square
+        """
         self.__size = size

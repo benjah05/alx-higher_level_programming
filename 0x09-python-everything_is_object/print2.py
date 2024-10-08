83fa42d5db5245afc4715ee57d685a96cfdc4d46
benjah05 <senzelebenjamine@gmail.com>
2024-10-08 12:51:17 -0700

    Python - Everything is an object(Part II)

---
+++ 0x09-python-everything_is_object/print2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""List add"""
+with open("15-answer.txt", "w") as f:
+    print("[1, 2, 3, 4]", file=f)
+"""Integer incrementation"""
+with open("16-answer.txt", "w") as f:
+    print("2", file=f)
+"""List incrementation"""
+with open("17-answer.txt", "w") as f:
+    print("[1, 2, 3, 4]", file=f)
+"""List assignation"""
+with open("18-answer.txt", "w") as f:
+    print("[4, 5, 6]", file=f)
+"""Tuple or not?"""
+with open("20-answer.txt", "w") as f:
+    print("No", file=f)
+"""Tuple or  not?"""
+with open("21-answer.txt", "w") as f:
+    print("Yes", file=f)
+"""Tuple or not?"""
+with open("22-answer.txt", "w") as f:
+    print("No", file=f)
+"""Tuple or not?"""
+with open("23-answer.txt", "w") as f:
+    print("Yes", file=f)
+"""Who am I?"""
+with open("24-answer.txt", "w") as f:
+    print("Yes", file=f)

b4789d4f33713af47ca0c835d5094dfec873ba3f
benjah05 <senzelebenjamine@gmail.com>
2024-10-08 13:27:54 -0700

    Python - Everything is an object(Part II)

--- 0x09-python-everything_is_object/print2.py
+++ 0x09-python-everything_is_object/print2.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """List add"""
 with open("15-answer.txt", "w") as f:
-    print("[1, 2, 3, 4]", file=f)
+    print("[1, 2, 3]", file=f)
 """Integer incrementation"""
 with open("16-answer.txt", "w") as f:
-    print("2", file=f)
+    print("1", file=f)
 """List incrementation"""
 with open("17-answer.txt", "w") as f:
     print("[1, 2, 3, 4]", file=f)
 """List assignation"""
 with open("18-answer.txt", "w") as f:
-    print("[4, 5, 6]", file=f)
+    print("[1, 2, 3]", file=f)
 """Tuple or not?"""
 with open("20-answer.txt", "w") as f:
-    print("No", file=f)
+    print("Yes", file=f)
 """Tuple or  not?"""
 with open("21-answer.txt", "w") as f:
     print("Yes", file=f)
@@ -25,4 +25,16 @@ with open("23-answer.txt", "w") as f:
     print("Yes", file=f)
 """Who am I?"""
 with open("24-answer.txt", "w") as f:
+    print("No", file=f)
+"""Tuple or not"""
+with open("25-answer.txt", "w") as f:
+    print("True", file=f)
+"""Empty is not empty"""
+with open("26-answer.txt", "w") as f:
+    print("False", file=f)
+"""Still the same?"""
+with open("27-answer.txt", "w") as f:
+    print("Yes", file=f)
+"""Same or not?"""
+with open("28-answer.txt", "w") as f:
     print("Yes", file=f)

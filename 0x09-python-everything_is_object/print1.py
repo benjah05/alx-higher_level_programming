35e712c7d1586741b8a1f2ed3f3b1b122aa1854d
benjah05 <senzelebenjamine@gmail.com>
2024-10-08 02:25:39 -0700

    Python - Everything is an object(Part I)

---
+++ 0x09-python-everything_is_object/print1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Who am I?"""
+with open("0-answer.txt", "w") as f:
+    print("type", file=f)
+"""Where are you?"""
+with open("1-answer.txt", "w") as f:
+    print("id", file=f)
+"""Right count"""
+with open("2-answer.txt", "w") as f:
+    print("No", file=f)
+"""Right count="""
+with open("3-answer.txt", "w") as f:
+    print("No", file=f)
+"""Right count ="""
+with open("4-answer.txt", "w") as f:
+    print("Yes", file=f)
+"""Right count =+"""
+with open("5-answer.txt", "w") as f:
+    print("No", file=f)
+"""Is equal"""
+with open("6-answer.txt", "w") as f:
+    print("True", file=f)
+"""Is the same"""
+with open("7-answer.txt", "w") as f:
+    print("True", file=f)
+"""Is really equal"""
+with open("8-answer.txt", "w") as f:
+    print("True", file=f)
+"""Is really the same"""
+with open("9-answer.txt", "w") as f:
+    print("False", file=f)
+"""And with a list, is it equal"""
+with open("10-answer.txt", "w") as f:
+    print("True", file=f)
+"""And with a list, is it the same"""
+with open("11-answer.txt", "w") as f:
+    print("False", file=f)
+"""And with a list, is it really equal"""
+with open("12-answer.txt", "w") as f:
+    print("True", file=f)
+"""And with a list, is it really the same"""
+with open("13-answer.txt", "w") as f:
+    print("True", file=f)
+"""List append"""
+with open("14-answer.txt", "w") as f:
+    print("[1, 2, 3, 4]", file=f)

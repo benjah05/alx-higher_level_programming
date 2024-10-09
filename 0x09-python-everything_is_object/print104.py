7ab986882bc8c96f146d1ca961d8eecbea68d4e1
benjah05 <senzelebenjamine@gmail.com>
2024-10-09 09:39:59 -0700

    FIX

--- 0x09-python-everything_is_object/print104.py
+++ 0x09-python-everything_is_object/print104.py
@@ -4,10 +4,10 @@ with open("104-line1.txt", "w") as f:
     print("1", file=f)
 """Int objects of the second line"""
 with open("104-line2.txt", "w") as f:
-    print("0", file=f)
+    print("11", file=f)
 """Line 3"""
 with open("104-line3.txt", "w") as f:
-    print("No", file=f)
+    print("Yes", file=f)
 """Line 4"""
 with open("104-line4.txt", "w") as f:
     print("Yes", file=f)

284d67da9e22029b5c62bed1194ba63a3c454c6b
benjah05 <senzelebenjamine@gmail.com>
2024-10-14 15:14:54 -0700

    Use isinstance(): check for object from inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -8,4 +8,7 @@ def is_kind_of_class(obj, a_class):
     inheritance from
     """
     def is_kind_of_class(obj, a_class):
-        return (isinstance(obj, a_class))
+        if isinstance(obj, a_class):
+            return (True)
+        else:
+            return (False)

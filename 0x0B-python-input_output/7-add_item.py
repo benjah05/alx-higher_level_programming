b2b4cd273244f61e093e557fbd3c8d1fcbf3e4db
benjah05 <senzelebenjamine@gmail.com>
2024-10-15 16:01:04 -0700

    Pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """Import JSON and save_to_join_file & load_from_json_file function"""
-
-
 import json
+from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-from sys import argv
 
 
 filename = "add_item.json"
 
 try:
     json_list = load_from_json_file(filename)
-except:
+except FileNotFoundError:
     json_list = []
 for arg in argv[1:]:
     json_list.append(arg)

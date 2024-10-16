ed135e05c0a831313cb72c645e11dad276172d4d
benjah05 <senzelebenjamine@gmail.com>
2024-10-16 05:18:27 -0700

    Parent/Child relationship

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,32 +1,7 @@
 #!/usr/bin/python3
-"""Represent class BaseGeometry"""
-
-
-class BaseGeometry:
-    """Define BaseGeometry"""
-    def area(self):
-        """BaseGeometry with method area
-        Raises:
-            Exception: area() is not implemented
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate "value"
-        Args:
-            name (str): name
-            value (str): value to validate
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than 0
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
 """Define class rectangle"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

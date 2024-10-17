1699a3ddd4bd89634ef0822e73f92c476afd2e63
benjah05 <senzelebenjamine@gmail.com>
2024-10-16 18:51:16 -0700

    Can I?

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,7 @@
 
 
 def add_attribute(class_name, attr_name="", attr_value=""):
+    """set attribute and check if it exists"""
     setattr(class_name, attr_name, attr_value)
     if hasattr(class_name, attr_name) is False:
         raise TypeError("can't add new attribute")

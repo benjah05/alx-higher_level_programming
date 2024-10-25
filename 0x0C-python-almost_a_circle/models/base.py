d5f1f5b6a2612b8e99d645feda5de9e2082430ba
benjah05 <senzelebenjamine@gmail.com>
2024-10-24 23:38:55 -0700

    return empty list not string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,7 +45,7 @@ class Base:
     def from_json_string(json_string):
         """Return the list of the JSON string representation json_string"""
         if json_string is None or len(json_string) == 0:
-            return ("[]")
+            return ([])
         else:
             return (json.loads(json_string))
 

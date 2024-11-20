48c21435e72bdf62f649b3343a3979418975a862
benjah05 <senzelebenjamine@gmail.com>
2024-11-20 00:36:00 -0800

    UTF8 & Temperature #n

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- Temperature #0
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+GROUP BY city ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,4 @@
+-- Temperature #1
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+WHERE month IN (7, 8)
+GROUP BY city ORDER BY avg_temp DESC LIMIT 3;

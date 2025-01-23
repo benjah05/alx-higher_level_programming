2d8f5fd629b1af1cfd02b44bafc5cfb5e261bba5
benjah05 <senzelebenjamine@gmail.com>
2025-01-23 00:44:32 -0800

    Adhere to module documentation rules

--- 0x0F-python-object_relational_mapping/check_doc.py
+++
@@ -1,8 +0,0 @@
-# Assuming a module named myscript.py exists
-import model_state as myscript
-
-if bool(myscript.__doc__):
-    print("The module has a docstring.")
-else:
-    print("The module does not have a docstring.")
-
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,26 +3,14 @@
     python file that contains the class definition
     of a State and an instance Base = declarative_base()
 """
-import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 Base = declarative_base()
-"""Instance"""
+""" Instance """
 
 
 class State(Base):
-    """
-    Class State that inherits from Base
-    args:
-        __tablename__: the name of the table
-        id: id column (primary key)
-        name: name column
-        engine: establish the connection with CL arguments
-    """
+    """ Class """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)

d32e1b3c39b109bfd5dbbee1e5ed26cd2f8a88dd
benjah05 <senzelebenjamine@gmail.com>
2025-01-23 00:53:27 -0800

    Adhere to module documentation rules

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -6,7 +6,7 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import asc
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """Add a new state"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
